8c767626feb4d97ecb6c4aa09f7eeae8ee4dde11
zhaojinxin <zhaojinxin@58.com>
2020-04-12 00:53:08 +0800

    按层遍历二叉树

--- src/main/java/algorithm/binaryTree/PrintByLevel.java
+++ src/main/java/algorithm/binaryTree/PrintByLevel.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @描述：按层打印二叉树
+ * @描述：按层遍历二叉树
  * @思路：进行宽度优先遍历，使用队列queue存储当前层和下一层的节点信息； 即： 从queue的头部弹出节点node；若node有孩子，以“先左后右”的形式从尾部进入queue.
  * @复杂度：时间O(N) 空间O(1)
  * @链接：https://www.nowcoder.com/practice/6a1815a85bfc411d9295bc017e6b6dbe
@@ -43,7 +43,6 @@ public class PrintByLevel {
      */
     public static void printByLevel2(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
-
         int level = 1;
         TreeNode last = root; //当前行的最右节点
         TreeNode nlast = null; //下一行的最右节点
@@ -51,7 +50,7 @@ public class PrintByLevel {
         queue.offer(root);
         System.out.print("\nLevel " + level++ + ":");
         while (!queue.isEmpty()) {
-            TreeNode curr = queue.poll();
+            TreeNode curr = queue.poll(); // -- 核心点
             System.out.print(curr.value + " ");
 
             if (curr.left != null) {
@@ -63,14 +62,47 @@ public class PrintByLevel {
                 nlast = curr.right;
             }
 
-            //换行时刻
+            //换行时刻 (注：其中!queue.isEmpty()判断可忽略，这里只是为了打印好看，才加的判断)
             if (curr == last && !queue.isEmpty()) {
                 last = nlast;
                 System.out.print("\nLevel " + level++ + ":");
             }
+        }
+    }
+
+
+    /*
+     * 宽度优先遍历进阶版 - 按层换行打印
+     * 如何确定换行？
+     * 标识出 当前层和下一层的分界位置
+     * 将队列中的值按层一次一次遍历；即：一次性将当前队列中的当前层的元素遍历完，再在一次性遍历下一层队列元素； 重点在于标识当前层和下一层的分界位置；
+     */
+    public static void printByLevel3(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        int level = 1;
+
+        queue.offer(root);
+        System.out.print("\nLevel " + level++ + ":");
+        while (!queue.isEmpty()) {
+            int length = queue.size();  // - 核心点  区分：标识当前层和下一层的分界位置
+            //换行时刻
+            for (int i = 0; i < length; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+                System.out.print(curr.value + " ");
+            }
+
+            //注：其中!queue.isEmpty()判断可忽略，这里只是为了打印好看，才加的判断
+            if (!queue.isEmpty()) {
+                System.out.print("\nLevel " + level++ + ":");
+            }
 
         }
-
     }
 
 
@@ -91,6 +123,8 @@ public class PrintByLevel {
 
         PrintByLevel.printByLevel1(root);
         PrintByLevel.printByLevel2(root);
+        PrintByLevel.printByLevel3(root);
+
     }
 
 
